ce85610eeb5a82b6010d8a6e161b9a4e37859f46
tyshchukRoman <roman.t@codelibry.dev>
2025-12-31 15:27:58 +0200

    init

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './styles.css'
+import '@styles/main.scss';
+import React from 'react';
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
